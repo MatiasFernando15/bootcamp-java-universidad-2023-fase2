880d43e783d0baaf35037c9f8abc0809943b7bf0
MatiasFernando15 <matiasdigiovanni15@gmail.com>
2023-10-23 18:58:58 -0300

    se modifican archivos por error

--- src/ar/com/educacionit/universidad/desafio/java/modulo/uno/EjercicioUno.java
+++ src/ar/com/educacionit/universidad/desafio/java/modulo/uno/EjercicioUno.java
@@ -8,57 +8,36 @@ public class EjercicioUno {
 		
 		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese una fecha en formato dd/mm/aaaa:");
-        String input = scanner.nextLine();
+        System.out.print("Ingrese la cantidad de alumnos a procesar: ");
+        int cantidadAlumnos = scanner.nextInt();
 
-        // Dividir la entrada en día, mes y año
-        String[] partes = input.split("/");
-        if (partes.length != 3) {
-            System.out.println("Formato de fecha incorrecto. Debe ser dd/mm/aaaa.");
-            return;
-        }
+        String[] nombres = new String[cantidadAlumnos];
+        String[] apellidos = new String[cantidadAlumnos];
+        int[] numExamenes = new int[cantidadAlumnos];
+        double[] notas = new double[cantidadAlumnos];
 
-        int dia = Integer.parseInt(partes[0]);
-        int mes = Integer.parseInt(partes[1]);
-        int año = Integer.parseInt(partes[2]);
+        for (int i = 0; i < cantidadAlumnos; i++) {
+            System.out.println("Ingrese los datos del alumno " + (i + 1));
+            System.out.print("Nombre: ");
+            nombres[i] = scanner.next();
+            System.out.print("Apellido: ");
+            apellidos[i] = scanner.next();
+            System.out.print("Número de exámenes: ");
+            numExamenes[i] = scanner.nextInt();
 
-        // Realizar las validaciones
-        if (año >= 1900 && año <= 2099) {
-            if (mes >= 1 && mes <= 12) {
-                if (mes == 2) {
-                    // Verificar si el año es bisiesto
-                    if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0)) {
-                        if (dia >= 1 && dia <= 29) {
-                            System.out.println("La fecha es correcta.");
-                        } else {
-                            System.out.println("Día incorrecto para febrero bisiesto.");
-                        }
-                    } else {
-                        if (dia >= 1 && dia <= 28) {
-                            System.out.println("La fecha es correcta.");
-                        } else {
-                            System.out.println("Día incorrecto para febrero no bisiesto.");
-                        }
-                    }
-                } else if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
-                    if (dia >= 1 && dia <= 30) {
-                        System.out.println("La fecha es correcta.");
-                    } else {
-                        System.out.println("Día incorrecto para este mes.");
-                    }
-                } else {
-                    if (dia >= 1 && dia <= 31) {
-                        System.out.println("La fecha es correcta.");
-                    } else {
-                        System.out.println("Día incorrecto para este mes.");
-                    }
-                }
-            } else {
-                System.out.println("Mes incorrecto.");
+            double sumaNotas = 0;
+            for (int j = 0; j < numExamenes[i]; j++) {
+                System.out.print("Nota " + (j + 1) + ": ");
+                double nota = scanner.nextDouble();
+                sumaNotas += nota;
             }
-        } else {
-            System.out.println("Año incorrecto.");
+            notas[i] = sumaNotas / numExamenes[i];
         }
+
+        // Aquí puedes realizar los cálculos necesarios para determinar los alumnos con calificación más alta, más baja, promocionados y los que deben recursar.
+        // Puedes utilizar bucles y variables auxiliares para realizar estos cálculos.
+
+        // Luego, imprime la información solicitada.
 
 	}
 
